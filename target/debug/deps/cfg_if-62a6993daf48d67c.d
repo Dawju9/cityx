bd6d7d01dd724fe1d9fef4c678a00fa7f3485c03
A ya Whitekali <dawju9@gmail.com>
2024-10-10 11:47:59 +0200

    esa

---
+++ target/debug/deps/cfg_if-62a6993daf48d67c.d
@@ -0,0 +1,5 @@
+F:\GithubProjekty\cityx\target\debug\deps\libcfg_if-62a6993daf48d67c.rmeta: C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
+
+F:\GithubProjekty\cityx\target\debug\deps\cfg_if-62a6993daf48d67c.d: C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
+
+C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs:
---
+++ target/debug/deps/version_check-93a0bcd5d464851b.d
@@ -0,0 +1,10 @@
+F:\GithubProjekty\cityx\target\debug\deps\libversion_check-93a0bcd5d464851b.rmeta: C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\version_check-0.9.5\src/lib.rs C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\version_check-0.9.5\src\version.rs C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\version_check-0.9.5\src\channel.rs C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\version_check-0.9.5\src\date.rs
+
+F:\GithubProjekty\cityx\target\debug\deps\libversion_check-93a0bcd5d464851b.rlib: C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\version_check-0.9.5\src/lib.rs C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\version_check-0.9.5\src\version.rs C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\version_check-0.9.5\src\channel.rs C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\version_check-0.9.5\src\date.rs
+
+F:\GithubProjekty\cityx\target\debug\deps\version_check-93a0bcd5d464851b.d: C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\version_check-0.9.5\src/lib.rs C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\version_check-0.9.5\src\version.rs C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\version_check-0.9.5\src\channel.rs C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\version_check-0.9.5\src\date.rs
+
+C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\version_check-0.9.5\src/lib.rs:
+C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\version_check-0.9.5\src\version.rs:
+C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\version_check-0.9.5\src\channel.rs:
+C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\version_check-0.9.5\src\date.rs:
