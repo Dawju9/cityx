b831d993000bd784d8d78f1aec6b431f6cdfb3b8
A ya Whitekali <dawju9@gmail.com>
2024-10-10 11:47:19 +0200

    esssa

---
+++ target/debug/build/winapi-a8e9e5e8899889a9/build_script_build-a8e9e5e8899889a9.d
@@ -0,0 +1,5 @@
+F:\GithubProjekty\cityx\target\debug\build\winapi-a8e9e5e8899889a9\build_script_build-a8e9e5e8899889a9.exe: C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs
+
+F:\GithubProjekty\cityx\target\debug\build\winapi-a8e9e5e8899889a9\build_script_build-a8e9e5e8899889a9.d: C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs
+
+C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs:
