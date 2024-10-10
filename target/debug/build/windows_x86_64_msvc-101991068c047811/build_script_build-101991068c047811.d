65e51359ff6816232c5b8197243d5098a0a6621f
A ya Whitekali <dawju9@gmail.com>
2024-10-10 11:49:59 +0200

    esa

---
+++ target/debug/build/windows_x86_64_msvc-101991068c047811/build_script_build-101991068c047811.d
@@ -0,0 +1,5 @@
+F:\GithubProjekty\cityx\target\debug\build\windows_x86_64_msvc-101991068c047811\build_script_build-101991068c047811.exe: C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.6\build.rs
+
+F:\GithubProjekty\cityx\target\debug\build\windows_x86_64_msvc-101991068c047811\build_script_build-101991068c047811.d: C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.6\build.rs
+
+C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.6\build.rs:
--- target/debug/deps/unicode_ident-73a82c1879845ec6.d
+++
@@ -1,8 +0,0 @@
-F:\GithubProjekty\cityx\target\debug\deps\libunicode_ident-73a82c1879845ec6.rmeta: C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.13\src/lib.rs C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.13\src\tables.rs
-
-F:\GithubProjekty\cityx\target\debug\deps\libunicode_ident-73a82c1879845ec6.rlib: C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.13\src/lib.rs C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.13\src\tables.rs
-
-F:\GithubProjekty\cityx\target\debug\deps\unicode_ident-73a82c1879845ec6.d: C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.13\src/lib.rs C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.13\src\tables.rs
-
-C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.13\src/lib.rs:
-C:\Users\dawju\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.13\src\tables.rs:
